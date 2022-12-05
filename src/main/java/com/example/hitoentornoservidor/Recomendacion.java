67a400f65a6efedad8a152d77b9e396fcd8807cf
fernando <fernando.trujillo@campusfp.es>
2022-12-05 15:13:48 +0100

    Commit Hito 5-12-22

---
+++ src/main/java/com/example/hitoentornoservidor/Recomendacion.java
@@ -0,0 +1,16 @@
+package com.example.hitoentornoservidor;
+
+public class Recomendacion {
+    private String pelicula;
+
+    public Recomendacion() {
+        this.pelicula = "Escribe aquí tu recomendación";
+    }
+    public String getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(String pelicula) {
+        this.pelicula = pelicula;
+    }
+}
--- src/main/java/com/example/hitoentornoservidor/Usuarios.java
+++ src/main/java/com/example/hitoentornoservidor/Usuarios.java
@@ -13,18 +13,18 @@ public class Usuarios {
 
     public Usuarios() {
         this.usuarios = new TreeMap<String, Usuario>();
-        this.usuarios.put("fernando", new Usuario("fernando", "123"));
+        this.usuarios.put("fernando", new Usuario("Fernando", "123"));
         this.usuarios.get("fernando").getRecomendaciones().add("Torrente 5");
         this.usuarios.get("fernando").getRecomendaciones().add("Scary Moovie");
         this.usuarios.get("fernando").getRecomendaciones().add("Spanish Moovie");
-        this.usuarios.put("sergio", new Usuario("sergio", "456"));
+        this.usuarios.put("sergio", new Usuario("Sergio", "456"));
         this.usuarios.get("sergio").getRecomendaciones().add("Casi 300");
         this.usuarios.get("sergio").getRecomendaciones().add("Ace Ventura");
         this.usuarios.get("sergio").getRecomendaciones().add("Wild hunting");
-        this.usuarios.put("alvaro", new Usuario("alvaro", "789"));
+        this.usuarios.put("alvaro", new Usuario("Alvaro", "789"));
         this.usuarios.get("alvaro").getRecomendaciones().add("8 apellidos vascos");
-        this.usuarios.get("alvaro").getRecomendaciones().add("padre no hay mas que uno");
-        this.usuarios.get("alvaro").getRecomendaciones().add("los manolos");
+        this.usuarios.get("alvaro").getRecomendaciones().add("Padre no hay mas que uno");
+        this.usuarios.get("alvaro").getRecomendaciones().add("Los manolos");
     }
 
     public TreeMap<String, Usuario> getUsuarios() {
